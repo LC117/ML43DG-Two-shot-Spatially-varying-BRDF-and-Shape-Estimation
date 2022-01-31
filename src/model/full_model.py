53ee909830b01e7993ace49b3a06b99cda617be7
Luca Scalerandi <Scalerandi.Luca@gmail.com>
2022-01-31 14:07:26 +0100

    update on full model

--- src/model/full_model.py
+++ src/model/full_model.py
@@ -1,3 +1,11 @@
+import numpy as np
+import cv2
+import pyexr
+import os
+import pathlib
+import time 
+import tqdm
+import torch
 
 from src.model.shape_model import ShapeNetwork
 from src.model.illumination_model import IlluminationNetwork
@@ -5,16 +13,82 @@ from src.model.svbrdf_model import SVBRDF_Network
 from src.model.joint_model import JointNetwork
 
 from utils.preprocessing_utils import read_image, read_mask
+from utils.preprocessing_utils import sRGBToLinear
+
+from utils.inference_renderer import Renderer, InferenceStage
+
+
 
 
 def full_inference(path_to_default_img, path_to_flash, path_to_mask):
     
-    # Default parameters for the inference:
-    parameters = [
-        ParameterNames.INPUT_1_LDR if isLdr else ParameterNames.INPUT_1,
-        ParameterNames.INPUT_2_LDR if isLdr else ParameterNames.INPUT_2,
-        ParameterNames.MASK,
-    ]
-    default_image =  read_image(path_to_default_img)
-    flash_image =  read_image(path_to_flash)
+    location = str(pathlib.Path(path_to_default_img).parent)
+    assert location == str(pathlib.Path(path_to_flash).parent)
+    
+    default_image =  sRGBToLinear(read_image(path_to_default_img)) # cam2
+    flash_image =  sRGBToLinear(read_image(path_to_flash)) # cam1
     mask = read_mask(path_to_mask)
+    
+    # Pass the Shape Network:
+    normal, depth = ShapeNetwork().forward((flash_image, default_image, mask))
+    # the variable normal is called "res" in the original code
+    normal, depth = normal[0], depth[0]
+    save(normal, str(location / r"normal_pred0.exr"))
+    save(depth, str(location / r"depth_pred0.exr"))
+    
+    # Pass the Illumination Network
+    sgs = IlluminationNetwork().forward((flash_image, default_image, mask, normal, depth))
+    save(sgs, str(location / r"sgs-pred.npy"))
+    
+    # Pass the SVBRDF Network:
+    diffuse, specular, roughness = SVBRDF_Network().forward(flash_image, default_image, mask, normal, depth)
+    save(diffuse, str(location / r"diffuse_pred0.exr"))
+    save(specular, str(location / r"specular_pred0.exr"))
+    save(roughness, str(location / r"roughness_pred0.exr"))
+    # Rendering the SVBRDF Output:
+    render = Renderer(InferenceStage.INITIAL_RENDERING).render(data=[(diffuse, specular, roughness, normal, depth, mask, sgs)])
+    save(render, str(location / f"rerender0.exr"))
+    
+def _is_hdr(path: str) -> bool:
+    _, ext = os.path.splitext(path)
+    return ext == ".exr" or ext == ".hdr"  
+    
+def save(
+    data: np.ndarray, save_path: str, grayscale: bool = False, alpha: bool = False
+):
+    """Saves the data to a specified path and handles all required extensions
+    Args:
+        img: The numpy RGB or Grayscale float image with range 0 to 1.
+        save_path: The path the image is saved to.
+        grayscale: True if the image is in grayscale, False if RGB.
+        alpha: True if the image contains transparency, False if opaque 
+    """
+    hdr = _is_hdr(save_path)
+    npy = os.path.splitext(save_path)[1] == ".npy"
+    if hdr:
+        pyexr.write(save_path, data)
+    elif npy:
+        np.save(save_path, data)
+    else:
+        asUint8 = (data * 255).astype(np.uint8)
+        if alpha:
+            if grayscale:
+                print("ALPHA AND GRAYSCALE IS NOT FULLY SUPPORTED")
+            proc = cv2.COLOR_RGBA2BGRA
+        elif not alpha and grayscale:
+            proc = cv2.COLOR_GRAY2BGR
+        else:
+            proc = cv2.COLOR_RGB2BGR
+
+        toSave = cv2.cvtColor(asUint8, proc)
+
+        cv2.imwrite(save_path, toSave)
+
+if __name__ == "__main__":
+    
+    full_inference(path_to_default_img=r"inference_images/examples/0/cam2.png", 
+                   path_to_flash=r"inference_images/examples/0/cam1.png", 
+                   path_to_mask=r"inference_images/examples/0/mask.png"
+                   )
+    
+    print("DONE!")
