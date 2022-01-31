fc2359c11f3f449f69a2c411d8edfe8c2cb3418e
Luca Scalerandi <Scalerandi.Luca@gmail.com>
2022-01-31 14:41:29 +0100

    full model up to rendering running

--- src/model/full_model.py
+++ src/model/full_model.py
@@ -3,6 +3,7 @@ import cv2
 import pyexr
 import os
 import pathlib
+import torch
 
 from src.model.shape_model import ShapeNetwork
 from src.model.illumination_model import IlluminationNetwork
@@ -18,19 +19,25 @@ from src.utils.inference_renderer import Renderer, InferenceStage
 
 def full_inference(path_to_default_img, path_to_flash, path_to_mask):
     
-    location = str(pathlib.Path(path_to_default_img).parent)
-    assert location == str(pathlib.Path(path_to_flash).parent)
+    location = pathlib.Path(path_to_default_img).parent
+    assert str(location) == str(pathlib.Path(path_to_flash).parent)
     
     default_image =  sRGBToLinear(read_image(path_to_default_img)) # cam2
     flash_image =  sRGBToLinear(read_image(path_to_flash)) # cam1
     mask = read_mask(path_to_mask, gray=False)
     
+    default_image = torch.Tensor(default_image[np.newaxis, ...]).permute(0, 3, 1, 2)
+    flash_image = torch.Tensor(flash_image[np.newaxis, ...]).permute(0, 3, 1, 2)
+    mask = torch.Tensor(mask[np.newaxis, np.newaxis, :, :])
+    
     # Pass the Shape Network:
     normal, depth = ShapeNetwork().forward((flash_image, default_image, mask))
     # the variable normal is called "res" in the original code
-    normal, depth = normal[0], depth[0]
-    save(normal, str(location / r"normal_pred0.exr"))
-    save(depth, str(location / r"depth_pred0.exr"))
+    # normal, depth = normal[0], depth[0]
+    save(np.transpose(normal[0].detach().numpy(), (1, 2, 0)), str(location / r"normal_pred0.exr"), also_as_png=True)
+    save(np.transpose(depth[0].detach().numpy(), (1, 2, 0)), str(location / r"depth_pred0.exr"), also_as_png=True)
+    
+    assert normal.max() <= 1. and depth.max() <= 1
     
     # Pass the Illumination Network
     sgs = IlluminationNetwork().forward((flash_image, default_image, mask, normal, depth))
@@ -45,12 +52,14 @@ def full_inference(path_to_default_img, path_to_flash, path_to_mask):
     render = Renderer(InferenceStage.INITIAL_RENDERING).render(data=[(diffuse, specular, roughness, normal, depth, mask, sgs)])
     save(render, str(location / f"rerender0.exr"))
     
+    assert diffuse.max() <= 1. and specular.max() <= 1 and roughness.max() <= 1
+    
 def _is_hdr(path: str) -> bool:
     _, ext = os.path.splitext(path)
     return ext == ".exr" or ext == ".hdr"  
     
 def save(
-    data: np.ndarray, save_path: str, grayscale: bool = False, alpha: bool = False
+    data: np.ndarray, save_path: str, grayscale: bool = False, alpha: bool = False, also_as_png: bool=True
 ):
     """Saves the data to a specified path and handles all required extensions
     Args:
@@ -63,6 +72,8 @@ def save(
     npy = os.path.splitext(save_path)[1] == ".npy"
     if hdr:
         pyexr.write(save_path, data)
+        if also_as_png:
+            cv2.imwrite(save_path.split(".")[0] + ".png", data * 255)
     elif npy:
         np.save(save_path, data)
     else:
@@ -79,6 +90,8 @@ def save(
         toSave = cv2.cvtColor(asUint8, proc)
 
         cv2.imwrite(save_path, toSave)
+        if also_as_png and len(save_path.split(".png")) == 1:
+            cv2.imwrite(save_path.split(".")[0]+".png", toSave)
 
 if __name__ == "__main__":
     
