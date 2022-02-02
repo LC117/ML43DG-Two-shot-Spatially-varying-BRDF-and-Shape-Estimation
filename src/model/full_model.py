38550725d175dbd8ab3fdb46e6fd1ea49625e5fa
Luca Scalerandi <Scalerandi.Luca@gmail.com>
2022-02-02 15:19:39 +0100

    final model update

--- src/model/full_model.py
+++ src/model/full_model.py
@@ -4,6 +4,7 @@ import pyexr
 import os
 import pathlib
 import torch
+import matplotlib.pyplot as plt
 
 from src.model.shape_model import ShapeNetwork
 from src.model.illumination_model import IlluminationNetwork
@@ -17,17 +18,21 @@ from src.utils.inference_renderer import Renderer, InferenceStage
 from src.utils.visualize_tools import save
 
 
-
-def full_inference(path_to_default_img, path_to_flash, path_to_mask):
+def full_inference(path_to_default_img, path_to_flash, path_to_mask, location=None):
     
     shape_net = ShapeNetwork().load_from_checkpoint(checkpoint_path=str(r"src/data/models/version_142/checkpoints/epoch=10-step=17016.ckpt"))
     ill_net = IlluminationNetwork().load_from_checkpoint(checkpoint_path=str(r"src/data/models/version_169/checkpoints/epoch=2-step=37124.ckpt"))
-    brdf_net = SVBRDF_Network().load_from_checkpoint(checkpoint_path=str(r"src/data/models/version_184/checkpoints/epoch=3-step=6187.ckpt"))
-    joint_net = JointNetwork()#.load_from_checkpoint(checkpoint_path=str(r"src/data/models/version_155/checkpoints/epoch=7-step=12375.ckpt"))
+    brdf_net = SVBRDF_Network().load_from_checkpoint(checkpoint_path=str(r"src/data/models/version_222/checkpoints/epoch=3-step=66667.ckpt"))
+    joint_net = JointNetwork().load_from_checkpoint(checkpoint_path=str(r"src/data/models/version_228/checkpoints/epoch=5-step=84857.ckpt"))
     
-    location = pathlib.Path(path_to_default_img).parent
-    assert str(location) == str(pathlib.Path(path_to_flash).parent)
+    shape_net.eval()
+    ill_net.eval()
+    brdf_net.eval()
+    joint_net.eval()
     
+    assert str(pathlib.Path(path_to_default_img).parent) == str(pathlib.Path(path_to_flash).parent) or location
+    location = pathlib.Path(path_to_default_img).parent if location is None else location
+       
     default_image =  sRGBToLinear(read_image(path_to_default_img)) # cam2
     flash_image =  sRGBToLinear(read_image(path_to_flash)) # cam1
     mask = read_mask(path_to_mask, gray=False)
@@ -49,6 +54,7 @@ def full_inference(path_to_default_img, path_to_flash, path_to_mask):
     sgs = ill_net.forward((flash_image, default_image, mask, normal, depth))
     save(sgs[0].detach().cpu().numpy(), str(location / r"sgs-pred.npy"))
     ill_net.render_and_store(sgs[0], path=str(location / r"sgs-pred.png"))
+    sgs_scatterplot(sgs[0].detach().cpu().numpy(), path=str(location / r"sgs-pred-scatterplot.png"))
     
     # Pass the SVBRDF Network:
     diffuse, specular, roughness = brdf_net.forward((flash_image, default_image, mask, normal, depth))
@@ -71,19 +77,39 @@ def full_inference(path_to_default_img, path_to_flash, path_to_mask):
     # Rendering the FINAL Output:
     render = Renderer(InferenceStage.INITIAL_RENDERING).render_all(
         data=(diffuse, specular, roughness, normal, depth, mask, sgs))
-    save(np.transpose(render.detach().cpu().numpy(), (1, 2, 0)), str(location / f"rerender_final.exr"))
+    save(np.transpose(render.detach().cpu().numpy(), (1, 2, 0)), str(location / f"rerender_final.exr"), also_as_png=True)
     
     assert diffuse.max() <= 1. and specular.max() <= 1 and roughness.max() <= 1
     
 def tensor_to_savable(tensor, transpose=True):
     return np.transpose(tensor[0].detach().cpu().numpy(), (1, 2, 0))
 
-
+def sgs_scatterplot(sgs, path):
+    fig = plt.figure()
+    # set the title to "targets"
+    fig.suptitle("SGS locations")
+    # create a 3d plot of the targets
+    ax = fig.add_subplot(111, projection='3d')
+    # plot the targets
+    ax.scatter(sgs[:, 0], sgs[:,  1], sgs[:, 2], c='r', marker='o')
+    # present the plot
+    plt.savefig(path, bbox_inches='tight')
+    
+    
 if __name__ == "__main__":
     
+    # Test Image theirs:
     full_inference(path_to_default_img=r"inference_images/examples/0/cam2.png", 
                    path_to_flash=r"inference_images/examples/0/cam1.png", 
-                   path_to_mask=r"inference_images/examples/0/mask.png"
+                   path_to_mask=r"inference_images/examples/0/mask.png",
+                   location = pathlib.Path(r"inference_images/examples_for_paper/testing_set")
                    )
+    
+    # Test Image ours:
+    # full_inference(path_to_default_img=r"inference_images/examples/our_own/own_normal.png", 
+    #                path_to_flash=r"inference_images/examples/our_own/own_flash.png", 
+    #                path_to_mask=r"inference_images/examples/our_own/own_mask.png",
+    #                location = pathlib.Path(r"inference_images/examples_for_paper/testing_set")
+    #                )
     
     print("DONE!")
